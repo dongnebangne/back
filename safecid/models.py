0cfd4f1bd8bea1768d398d1cdadc66b104958860
jiyoung <gyjeong021@gmail.com>
2024-08-29 08:02:31 +0900

    [Fix] 대학명 주소 검색 데이터 모델 정의, api 구현

--- safecid/models.py
+++ safecid/models.py
@@ -7,3 +7,12 @@ class Address(models.Model):
 
     def __str__(self):
         return f"{self.sido} {self.sigungu} {self.eupmyeondong}"
+
+class University(models.Model):
+    univ_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    lon = models.FloatField(null=True, blank=True)
+    lat = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.univ_name
--- safecid/urls.py
+++ safecid/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import GetWMSLayer, GetLegend, SidoView, SigunguView, EupmyeondongView, GetCoordinatesFromAddress, GetAddressFromCoordinates
+from .views import (GetWMSLayer, GetLegend, SidoView, SigunguView, EupmyeondongView,
+                    LocationView, UniversityView, UniversityCoordinates, GetCoordinatesFromAddress, GetAddressFromCoordinates)
 
 urlpatterns = [
     path('get-wms-layer/', GetWMSLayer.as_view(), name='get-wms-layer'),
@@ -7,6 +8,9 @@ urlpatterns = [
     path('sido/', SidoView.as_view(), name='sido-list'),
     path('sigungu/<str:sido>/', SigunguView.as_view(), name='sigungu-list'),
     path('eupmyeondong/<str:sido>/<str:sigungu>/', EupmyeondongView.as_view(), name='eupmyeondong-list'),
+    path('locations/', LocationView.as_view(), name='location-list'),
+    path('universities/<str:location>/', UniversityView.as_view(), name='university-list'),
+    path('university/coordinates/<str:name>/', UniversityCoordinates.as_view(), name='university-coordinates'),
     path('coordinates/', GetCoordinatesFromAddress.as_view(), name='get_coordinates'),
-    path('address/', GetAddressFromCoordinates.as_view(), name='get_address'),
+    path('address/', GetAddressFromCoordinates.as_view(), name='get_address')
 ]
