915494b44d34d3291168b86ea9e99a41faa353bb
jiyoung <gyjeong021@gmail.com>
2024-10-03 07:04:21 +0900

    [Fix] setting.py cors 코드 수정

--- safecidProject/settings.py
+++ safecidProject/settings.py
@@ -66,8 +66,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
-    "43.201.248.203"
-    "43.201.102.175"
+    "http://43.201.248.203"
+    "http://43.201.102.175"
 ]
 
 ROOT_URLCONF = 'safecidProject.urls'
