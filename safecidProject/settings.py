7e29efd09e41ac13579bd7bbd84f245ea984338c
Yewon0106 <yooyy0106@naver.com>
2024-10-04 16:53:04 +0900

    [Fix] ai settings debug true

--- safecidProject/settings.py
+++ safecidProject/settings.py
@@ -23,7 +23,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+#DEBUG = env('DEBUG')
+#윗줄 기존거
+#아랫줄이 수정한 것
+DEBUG = True ####이거 이렇게 바껴야 사진 출력됨!!
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
