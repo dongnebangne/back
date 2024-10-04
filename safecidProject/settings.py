8adf2508dd7955660180195c573238dfced55825
jiyoung <gyjeong021@gmail.com>
2024-10-04 16:49:29 +0900

    [Fix] setting.py 코드 수정

--- safecidProject/settings.py
+++ safecidProject/settings.py
@@ -52,22 +52,23 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8000",
     "http://43.203.254.34",
-    "http://43.201.102.175"
+    "http://43.201.102.175",
+    "https://43.203.254.34",
+    "https://43.201.102.175",
 ]
 
 ROOT_URLCONF = 'safecidProject.urls'
