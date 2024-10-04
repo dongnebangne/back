40edf795a0bd2d504b559a35657ae5f34714a743
Jiyoung <gyjeong021@gmail.com>
2024-10-04 17:49:23 +0900

    Update settings.py

--- safecidProject/settings.py
+++ safecidProject/settings.py
@@ -70,10 +70,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
 #     "http://localhost:8000",
-#     "http://43.203.254.34:3000",
-#     "http://43.201.102.175",
-#     "https://43.203.254.34",
-#     "https://43.201.102.175",
+#     "http://3.36.232.193:3000",
+#     "http://3.35.119.183",
 # ]
 
 ROOT_URLCONF = 'safecidProject.urls'
